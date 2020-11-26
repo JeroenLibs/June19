d7deff63cda47d6dcbb8581cc2e7718ac721ca15
Jeroen Broks <facebook@tbbs.nl>
2020-11-26 23:34:50 +0100

    Flash a window

--- Test_June19.cpp
+++ Test_June19.cpp
@@ -1,6 +1,12 @@
+#include <SDL.h>
+#include <SDL_main.h>
+#include <TQSG.hpp>
 #include "src/june19.hpp"
 
+using namespace TrickyUnits;
 
 int main(int argcount, char** args) {
+	TQSG_Init("Test June19");
+	TQSG_Close();
 	return 0;
 }
