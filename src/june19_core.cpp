e2a2bc2905d21c8f73d1210f3a719feab0b8f023
Jeroen Broks <facebook@tbbs.nl>
2020-11-26 21:17:51 +0100

    Basis of the core, but there's still a crapload to do here!

---
+++ src/june19_core.cpp
@@ -0,0 +1,202 @@
+#include <TQSG.hpp>
+#include <TQSE.hpp>
+
+#include "june19_core.hpp"
+
+using namespace std;
+using namespace TrickyUnits;
+
+// Macros are evil! I know!
+// But I am evil, as well!
+
+#define ggc(ac,tc,rc)\
+	_error = "";\
+	if (pure)\
+		return ac;\
+	switch (tx) { \
+		case j19ctype::Absolute: return ac;\
+		case j19ctype::Percent: \
+			if (!parent) { _error = "No parent"; return 0; }\
+			return (int)floor(parent->rc() * (ac / 100)); \
+		default: \
+			_error = "Unknown coordinate calculation type! (bug?)"; \
+			return 0; \
+	}
+
+#define sgc(ac,tc)\
+	if (kind == j19kind::Unknown || kind == j19kind::EntireScreen || kind == j19kind::WorkScreen) { _error = "This gadget type may not be modified"; return; } \
+	ac = value; tc = t;
+
+
+
+namespace june19 {
+
+	static string _error{ "" };
+	static j19gadget _Screen;
+	static j19gadget _WorkScreen;
+
+	static void DrawScreen(j19gadget* self) {} // This just had to exist, that's all!
+	static bool DS_Success{ j19gadget::RegDraw(j19kind::EntireScreen,DrawScreen) };
+
+	std::map<j19kind, j19draw> j19gadget::HowToDraw;
+
+	bool j19gadget::RegDraw(j19kind k, j19draw v) {
+		_error = "";
+		if (HowToDraw.count(k)) {
+			_error = "Duplicate kind draw registration";
+			return false;
+		}
+		HowToDraw[k] = v;
+		return true;
+	}
+
+	void j19gadget::SetKind(j19kind value) { 
+		_error = "";
+		if (kind != j19kind::Unknown)
+			_error = "Kind already set for this gadget";
+		kind = value;
+	}
+
+	void j19gadget::SetParent(j19gadget* value) {
+		if (parent)
+			return;
+		else {
+			parent = value;
+			value->kids.push_back(this);
+		}
+	}
+
+	void j19gadget::X(int value) { X(value, tx); }
+
+	void j19gadget::X(int value, j19ctype t) {
+		/* proto
+		if (kind == j19kind::Unknown || kind == j19kind::EntireScreen || kind == j19kind::WorkScreen) { _error = "This gadget type may not be modified"; return; }
+		_x = value; tx = t;
+		// */
+		sgc(_x, tx);
+	}
+
+	int j19gadget::X(bool pure) {
+		/* proto
+		_error = "";
+		if (pure)
+			return _x;
+		switch (tx) {
+		case j19ctype::Absolute: return _x;
+		case j19ctype::Percent:
+			if (!parent) { _error = "No parent"; return 0; }
+			return (int)floor(parent->W() * (_x / 100));
+		default:
+			_error = "Unknown coordinate calculation type! (bug?)";
+			return 0;
+		}
+		*/
+		if (kind == j19kind::EntireScreen || kind == j19kind::WorkScreen) return 0;
+		ggc(_x, tx, W)
+	}
+	void j19gadget::W(int value) { W(value, tw); }
+	void j19gadget::W(int value, j19ctype t) { sgc(_w, tw); }
+	int j19gadget::W(bool pure) {
+		switch (kind) {
+		case j19kind::EntireScreen:
+		case j19kind::WorkScreen:
+			return TQSG_ScreenWidth();
+		default:
+			ggc(_w, tw, W);
+		}
+	}
+	void j19gadget::Y(int value) { Y(value, ty); }
+	void j19gadget::Y(int value, j19ctype t) { sgc(_y, ty); }
+	int j19gadget::Y(bool pure) {
+		switch (kind) {
+		case j19kind::EntireScreen:
+			return 0;
+		case j19kind::WorkScreen:
+			_error = "Workscreen not fully supported yet!";
+			return 0; // Pulldown menus and status bar can play a role here!
+		default:
+			ggc(_y, ty, H);
+		}
+	}
+	void j19gadget::H(int value) { H(value, th); }
+	void j19gadget::H(int value, j19ctype t) { sgc(_h, th); }
+	int j19gadget::H(bool pure) {
+		switch (kind) {
+		case j19kind::EntireScreen:
+			return TQSG_ScreenHeight();
+		case j19kind::WorkScreen:
+			_error = "Workscreen not fully supported yet!";
+			return TQSG_ScreenWidth(); // Pulldown menus and status bar can play a role here!
+		default:
+			ggc(_h, th, H);
+		}
+	}
+
+	int j19gadget::DrawX() {
+		switch (kind) {
+		case j19kind::EntireScreen:
+		case j19kind::WorkScreen:
+			return 0;
+		default:
+			return X() + parent->X();
+		}
+
+	}
+
+	int j19gadget::DrawY() {
+		switch (kind) {
+		case j19kind::EntireScreen:
+			return 0;
+		case j19kind::WorkScreen:
+			_error = "Workscreen not fully supported yet!";
+			return 0; // Pulldown menus and status bar can play a role here!
+		default:
+			return Y() + parent->Y();
+		}
+	}
+
+	void j19gadget::DetachParent() {
+		parent->RemoveKid(this);
+	}
+
+	void j19gadget::RemoveKid(j19gadget* kid) {
+		_error = "";
+		int f{ -1 };
+		do {
+			if (f >= kids.size()) { _error = "Kid to remove not found!"; return; }
+		} while (kids[f++] != kid);
+		kids.erase(kids.begin() + f);
+	}
+
+	j19gadget* Screen() {
+		static bool initiated{ false };
+		if (!initiated) {
+			WorkScreen();
+			_Screen.SetKind(j19kind::EntireScreen);
+			initiated = true;
+		}
+		return &_Screen;
+	}
+
+	j19gadget* WorkScreen() {
+		static bool initiated{ false };
+		if (!initiated) {
+			_WorkScreen.SetParent(Screen());
+			_WorkScreen.SetKind(j19kind::WorkScreen);
+			initiated = true;
+		}
+		return &_WorkScreen;
+	}
+
+	void FreeGadget(j19gadget* gadget) {
+		_error = "";
+		if (gadget == &_Screen || gadget == &_WorkScreen) {
+			_error = "Screen and WorkScreen cannot be disposed!";
+			return;
+		}
+		gadget->KillKids();
+		gadget->DetachParent();
+		delete gadget;
+	}
+
+}
---
+++ src/june19_core.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+
+#define j19byte unsigned char
+
+#define j19nullgadget(gadget) {FreeGadget(gadget); gadget=nullptr;}
+
+namespace june19 {
+
+	enum class j19kind {
+		Unknown,
+		EntireScreen,
+		WorkScreen,
+		Group,
+		Label,
+		TextField,
+		RadioButton,
+		CheckBox,
+		ListBox
+	};
+
+	enum class j19ctype {  Absolute, Percent };
+
+	class j19gadget;
+
+	typedef void (*j19draw)(j19gadget* g);
+
+	class j19gadget {
+	private:
+		j19gadget* parent{ nullptr };
+		std::vector<j19gadget*> kids;
+		j19kind kind{ j19kind::Unknown };
+		int _x{ 0 }, _y{ 0 }, _w{ 100 }, _h{ 100 };
+		j19ctype tx{ j19ctype::Absolute }, ty{ j19ctype::Absolute }, tw{ j19ctype::Absolute }, th { j19ctype::Absolute };
+		static std::map<j19kind, j19draw> HowToDraw;
+	public:
+		static bool RegDraw(j19kind k, j19draw v);
+
+		void SetKind(j19kind value); // Please note this only works once, and most creation fuctions take care of this automatically.
+		void SetParent(j19gadget* value); // Please note this only works once, and most creation fuctions take care of this automatically.
+
+		void X(int value);
+		void X(int value, j19ctype t);
+		int X(bool pure = false);
+
+		void W(int value);
+		void W(int value, j19ctype t);
+		int W(bool pure = false);
+
+		void Y(int value);
+		void Y(int value, j19ctype t);
+		int Y(bool pure = false);
+
+		void H(int value);
+		void H(int value, j19ctype t);
+		int H(bool pure = false);
+
+		int DrawX();
+		int DrawY();
+
+		// These methods should NEVER be called directly! FreeGadget() needs them
+		void KillKids(); // Called by "FreeGadget". Don't call this directly unless you know what you are doing
+		void DetachParent(); // Remove Gadget from parent's kids. NEVER call this yourself unless you know what you doing! FreeGadget needs this method, that's all!
+		void RemoveKid(j19gadget *kid); // Remove kid from parent. NEVER call this yuourself, unless youknow what you are doing! FreeGadget needs this methid, and that's all!
+
+	};
+
+	j19gadget* Screen();
+	j19gadget* WorkScreen();
+	void FreeGadget(j19gadget* gadget); // Disposes a gadget and all its children. 
+
+}
