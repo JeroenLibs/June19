9d54d8dd1a886d76139abe9d498f8d571668d1b4
Jeroen Broks <facebook@tbbs.nl>
2020-11-28 11:41:56 +0100

    vUpdate

--- src/june19_picture.cpp
+++ src/june19_picture.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // src/june19_picture.cpp
 // June 19
-// version: 20.11.27
+// version: 20.11.28
 // Copyright (C) 2020 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
